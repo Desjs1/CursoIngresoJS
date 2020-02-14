e6f111523b1915f70d0b62cc24f3d3ce504ed648
Desjs1 <60760258+Desjs1@users.noreply.github.com>
2020-02-13 22:23:01 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad=parseInt(document.getElementById("edad").value);
+
+if(edad < 13) {
+    alert("Niño");
+}
+else if (edad <= 17){
+    alert("Adolescente");
+}
+else {
+    alert("Adulto");
+}
 
 
 
