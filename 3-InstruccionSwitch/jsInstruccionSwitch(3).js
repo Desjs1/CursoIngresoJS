6f3a4a628bd1df1fc2d6b9d9b7495f88bf235d81
Desjs1 <60760258+Desjs1@users.noreply.github.com>
2020-02-14 21:52:40 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,36 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+switch(mes){
+    case "Enero":
+    alert("Este mes tiene 30 o más días");
+    break;
+    case "Febrero":
+    alert(". Este mes no tiene más de 29 días");
+    break;
+    case"Marzo":
+    case"Abril":
+    case "Mayo":
+    case "Junio":
+    case"Julio":
+    case"Agosto":
+    case "Septiembre":
+    case "Octubre":
+    case"Noviembre":
+    case"Diciembre":
+    alert("Este mes tiene 30 o más días");
+    break;
+}
 
-
-}//FIN DE LA FUNCIÓN
+ /*if(mes == "Febrero"){
+     alert(" Este mes no tiene más de 29 días");
+ }
+ else {
+ alert("Este mes tiene 30 o más días");
+ }
+}
+//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,11 +1,29 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+switch(mes) {
+    case "Enero":
+    case"Marzo":
+    case"Mayo":
+    case"Julio":
+    case"Agosto":
+    case "Octubre":
+    case"Diciembre":
+    alert("tiene 31 días");
+    break;
+    case "Febrero":
+    alert("tiene 28 días");
+    break;
+    default:
+    alert("tiene 30 dias")
+
+}
+
+
 
 
 
