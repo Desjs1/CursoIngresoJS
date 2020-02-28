1ebac453dbeddb29d0b2f3c9ab9d7834f8f5d8e2
Desjs1 <60760258+Desjs1@users.noreply.github.com>
2020-02-28 16:10:05 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero;
+	var maximo=10;
+	var minimo=0;
 
+	numero= Math.floor(Math.random()*((maximo + 1) - minimo) + minimo);
+
+	if(numero >=9){
+		alert("nota excelente: "+ numero);
+
+		
+	}else if(numero < 4){
+		alert("Vamos la proxima se puede: "+ numero);
+
+	}else{
+		alert("aprobó: " + numero);
+	}
+		
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -5,7 +5,7 @@ function mostrar()
 	var maximo = 10;
 	var minimo = 1;
 
-	numero= Math.floor(Math.ramdon()*((maximo + 1) - minimo) + minimo);
+	numero= Math.floor(Math.random()*((maximo + 1) - minimo) + minimo);
 	alert(numero);
 
 }//FIN DE LA FUNCIÓN
